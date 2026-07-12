ae836fa28ca7ba257dbff752b39e274115b8e6fb
Yoon A Limsuwan (美美) <msps4u@gmail.com>
2026-07-12 13:26:43 +0700

    Update developer information in ngo_physics_one.py

--- ngo_physics_one.py
+++ ngo_physics_one.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # NGO PHYSICS ONE — Production-Grade Structural GNO Physics Surrogate
 # =============================================================================
-# Developer    : Yoon A Limsuwan / MSPS NETWORK
+# Developer    : PAI , Yoon A Limsuwan / MSPS NETWORK
 #                MY SOUL MOVE BY POWER OF HOLY SPIRIT
 # Organization : MSPS NETWORK
 # ORCID        : 0009-0008-2374-0788
